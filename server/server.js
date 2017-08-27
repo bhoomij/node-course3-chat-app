ceb6946540ba1e4e6d5e53f5ba8ed38e8dfebf45
bhoomi <bhoomi>
2017-08-28 01:26:23 +0530

    Add broadcast for newMessage event

--- server/server.js
+++ server/server.js
@@ -13,7 +13,16 @@ var io = socketIO(server);
 
 io.on('connection', (socket) => {
     console.log('New user is connected');
-    
+
+    socket.on('createMessage', (message) => {
+        console.log('createMessage', message);
+        socket.broadcast.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            createdAt: new Date().getTime()
+        });
+    });
+
     socket.on('disconnect', () => {
         console.log('User was disconnected')
     });
