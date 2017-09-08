4a14af8d7483606bbaecb28cf089296db65e5539
bhoomi <bhoomi>
2017-09-08 22:54:01 +0530

    Add css for chat app

--- public/js/index.js
+++ public/js/index.js
@@ -21,13 +21,14 @@
         $('#messages').append(li);
     });
 
+    var messageTextbox = $('[name=message]');
     $('#message-form').on('submit', function(e) {
         e.preventDefault();
         socket.emit('createMessage', {
             from: 'User',
-            text: $('[name=message]').val()
+            text: messageTextbox.val()
         }, function() {
-
+            messageTextbox.val('');
         });
     });
 
@@ -38,13 +39,15 @@
         {
             return alert('Your browser does not support geolocation');
         }
-
+        locationButton.attr('disabled', 'disabled').text('Sending location...');
         navigator.geolocation.getCurrentPosition(function(position) {
+            locationButton.removeAttr('disabled').text('Send location');
             socket.emit('createLocationMessage', {
                 latitude: position.coords.latitude,
                 longitude: position.coords.longitude
             });
         }, function() {
+            locationButton.removeAttr('disabled').text('Send location');
             alert('Unable to fetch location');
         });
     });
